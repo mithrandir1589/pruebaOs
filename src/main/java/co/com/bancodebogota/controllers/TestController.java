4b968ac3b2d29363d031f2e1fc4fc5fabc056409
mquint2 <miquint2@bancodebogota.com.co>
2019-01-11 09:38:49 -0500

    Primer commit

--- src/main/java/co/com/bancodebogota/controllers/TestController.java
+++ src/main/java/co/com/bancodebogota/controllers/TestController.java
@@ -1,18 +1,21 @@
 package co.com.bancodebogota.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-	
+	@Value("#{systemEnvironment['MY_EXTERNAL_ENDPOINT']}")
 	private String enviromentVariable;
 	
 	@GetMapping("/getVariable")
 	public String returnVariable() {
-		this.enviromentVariable = "valor nuevo";
 		return this.enviromentVariable;
-		
 	}
 
+	@GetMapping("/getVariable2")
+	public String returnVariable2() {
+		return "Este si se ve";
+	}
 }
